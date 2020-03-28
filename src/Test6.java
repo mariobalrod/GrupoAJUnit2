cf94020406a84d34ba6ba9a3efa000c0d3fa7ca3
mariobalrod <marioballestero.rdg@gmail.com>
2020-03-28 18:15:20 +0100

    Pruebas de la funcion 6 realizadas #6

--- src/Test6.java
+++ src/Test6.java
@@ -1,3 +1,6 @@
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -33,12 +36,38 @@ class Test6 {
 	
 	// PRUEBAS
 	
-	@DisplayName("")
+	@DisplayName("Prueba de Caja Blanca de Cobertura de Caminos 1")
 	@Test
 	/*
-	 * 
+	 * En primer lugar paso un array con longitud 0, 
+	 * para que no entre en el bucle for.
+	 * Esto dara como resultado el mismo array, es decir, 
+	 * no se ve modificado
 	 */
 	void prueba1() {
-		
+		double[] array = new double[0];
+		assertArrayEquals(array, f.modArrayDoubles(array));
+	}
+	
+	@DisplayName("Prueba de Caja Blanca de Cobertura de Caminos 2")
+	@Test
+	/*
+	 * En esta pasare un array de longitud 1 para que solo entre
+	 * en la condicion de par.
+	 */
+	void prueba2() {
+		double[] array = {1};
+		assertArrayEquals(array, f.modArrayDoubles(array));
+	}
+	
+	@DisplayName("Prueba de Caja Blanca de Cobertura de Caminos 3")
+	@Test
+	/*
+	 * En esta pasare un array de longitud 2 para que entre
+	 * tambien en la condicion de impar
+	 */
+	void prueba3() {
+		double[] array = {1, 1};
+		assertArrayEquals(array, f.modArrayDoubles(array));
 	}
 }
