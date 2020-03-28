e23d88af2838ac16adda3863de259492f6f6f200
mariobalrod <marioballestero.rdg@gmail.com>
2020-03-28 17:44:44 +0100

    Pruebas de la funcion 4 realizadas #4

--- src/Test4.java
+++ src/Test4.java
@@ -1,3 +1,5 @@
+import static org.junit.Assert.assertEquals;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -33,12 +35,38 @@ class Test4 {
 	
 	// PRUEBAS
 	
-	@DisplayName("")
+	@DisplayName("Prueba de Caja Negra Con cadenas vacias")
 	@Test
 	/*
-	 * 
+	 * Prueba en la que le paso cadenas vacias 
 	 */
 	void prueba1() {
-		
+		assertEquals("", f.cadenas("", "", ""));
+	}
+	
+	@DisplayName("Prueba de Caja Negra Con cadenas de 1 caracter")
+	@Test
+	/*
+	 * Prueba en la que le paso cadenas de un caracter
+	 */
+	void prueba2() {
+		assertEquals("bc", f.cadenas("a", "b", "c"));
+	}
+	
+	@DisplayName("Prueba de Caja Negra con Valores aleatorios")
+	@Test
+	/*
+	 * Prueba en la que paso cadenas que he elegido
+	 * sin nungun sentido (al azar), para comprobar el
+	 * comportamiento de la funcion
+	 * 
+	 * Le paso:
+	 * 
+	 * 	- abc
+	 * 	- ABC
+	 *  - aaa
+	 */
+	void prueba3() {
+		assertEquals("bc", f.cadenas("abc", "ABC", "aaa"));
 	}
 }
