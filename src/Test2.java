a4af793c5b192fa67d8aeafa82e64d16e1096e49
mariobalrod <marioballestero.rdg@gmail.com>
2020-03-28 17:01:18 +0100

    Pruebas de la funcion 2 realizadas #2

--- src/Test2.java
+++ src/Test2.java
@@ -1,3 +1,6 @@
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -33,12 +36,45 @@ class Test2 {
 	
 	// PRUEBAS
 	
-	@DisplayName("")
+	@DisplayName("Prueba de Caja Negra de Limite Superior")
 	@Test
 	/*
-	 * 
+	 * Se evalua el comportamiento de la funcion pasandole
+	 * el limite superior de la condicion que en este caso
+	 * es 200
 	 */
 	void prueba1() {
-		
+		assertTrue(f.numeroIntervalo(200));
+	}
+	
+	@DisplayName("Prueba de Caja Negra de Limite Superior +1")
+	@Test
+	/*
+	 * Se evalua el comportamiento de la funcion pasandole
+	 * el limite superior+1 de la condicion que en este caso
+	 * es 201
+	 */
+	void prueba2() {
+		assertFalse(f.numeroIntervalo(201));
+	}
+	
+	@DisplayName("Prueba de Caja Negra de Limite Inferior")
+	@Test
+	/*
+	 * Se evalua el comportamiento de la funcion pasandole
+	 * el limite inferior que es el 100
+	 */
+	void prueba3() {
+		assertTrue(f.numeroIntervalo(100));
+	}
+	
+	@DisplayName("Prueba de Caja Negra de Limite Inferio -1")
+	@Test
+	/*
+	 * Se evalua el comportamiento de la funcion pasandole
+	 * el limite inferior -1 que es el 99
+	 */
+	void prueba4() {
+		assertFalse(f.numeroIntervalo(99));
 	}
 }
