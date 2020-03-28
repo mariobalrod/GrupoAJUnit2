5f10adc18a74f348068620f2ac9a98ce6dda1d89
mariobalrod <marioballestero.rdg@gmail.com>
2020-03-28 15:22:40 +0100

    Test 3 preparado

---
+++ src/Test3.java
@@ -0,0 +1,44 @@
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class Test3 {
+	
+	static Funciones f = null;
+	static int numeroPruebasRealizadas = 0;
+	@BeforeAll
+	static void prepararPruebas() {
+		System.out.println("Preparando Tests");
+		f = new Funciones();
+	}
+	@BeforeEach
+	private void preparaPruebaEjecutar(){
+		numeroPruebasRealizadas++;
+		System.out.println("Ejecutando test "+ numeroPruebasRealizadas);
+	}
+	@AfterEach
+	private void terminadaPrueba(){
+		System.out.println("Se ha ejecutado el test: "+numeroPruebasRealizadas);
+	}
+	@AfterAll
+	static void terminarPruebas() {
+		System.out.println("Se han terminado de ejecutar todas las pruebas");
+		f = null;
+	}
+	
+	//---------------------------------------------------------------------------------
+	
+	// PRUEBAS
+	
+	@DisplayName("")
+	@Test
+	/*
+	 * 
+	 */
+	void prueba1() {
+		
+	}
+}
