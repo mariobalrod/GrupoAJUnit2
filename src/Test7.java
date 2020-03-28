e5e3f1e3c813d661978260ca29f46ba87f8c22f1
mariobalrod <marioballestero.rdg@gmail.com>
2020-03-28 18:27:09 +0100

    Pruebas de la funcion 7 realizadas #7

--- src/Test7.java
+++ src/Test7.java
@@ -1,3 +1,7 @@
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -33,12 +37,41 @@ class Test7 {
 	
 	// PRUEBAS
 	
-	@DisplayName("")
+	@DisplayName("Prueba de Caja Negra de Array Vacio")
 	@Test
 	/*
-	 * 
+	 * Le paso un Array vacio. Esto nos devuelve como
+	 * expresion resultado un ArrayList vacio tambien.
 	 */
 	void prueba1() {
-		
+		assertEquals(new ArrayList<String>(), f.recorte(new String[0]));
+	}
+	
+	@DisplayName("Prueba de Caja Negra de Arrays con un elemento")
+	@Test
+	/*
+	 * Le paso un Array con un solo elemento, es decir, un
+	 * array de longitud 1
+	 */
+	void prueba2() {
+		String[] palabras = {"aaaaaaa"};
+		ArrayList<String> resultado = new ArrayList<String>();
+		resultado.add("aaaa");
+		assertEquals(resultado, f.recorte(palabras));
+	}
+	
+	@DisplayName("Prueba de Caja Negra de Arrays con varios elemento")
+	@Test
+	/*
+	 * Le paso un Array con varios elementos, en este caso un array
+	 * de longitud 3
+	 */
+	void prueba3() {
+		String[] palabras = {"aaaaaaa", "aaaaaaa", "aaaaaaa"};
+		ArrayList<String> resultado = new ArrayList<String>();
+		resultado.add("aaaa");
+		resultado.add("aaaa");
+		resultado.add("aaaa");
+		assertEquals(resultado, f.recorte(palabras));
 	}
 }
